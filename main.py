977bef0cd989abe16caf9b0d4c4db8625d045c5c
Nathan Kuik <nathan.kuik@gmail.com>
2018-08-02 22:08:19 +0200

    Extremely simple healthpoint added

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ import config.configuration as config
 
 app = Sanic(__name__)
 
+
 @app.route("/")
 async def root(request):
     return json({'hello': 'world'})
+
+@app.route("/healthcheck")
+async def root(request):
+
+    def addition_works():
+        if 1 + 1 == 2:
+            return True, "addition works"
+        else:
+            return False, "the universe is broken"
+
+    return json(addition_works())
 
 @app.route("/listening", methods=['POST'])
 async def listening(request):
